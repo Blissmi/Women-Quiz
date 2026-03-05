917d5cf0a5b89e7e2564e370919fdea512095cca
ZaraNaeem1234 <zara.naeem@remelda.com>
2026-03-05 16:33:25 +0500

    Allowed host.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import path from 'path'
 export default defineConfig({
   base: '/women-event/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['myblissmi.com'],
+  },
   resolve: {
     alias: {
       '@/': path.resolve(__dirname, 'src') + '/',
     },
   },
 })
+     
