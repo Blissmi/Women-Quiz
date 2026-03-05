8c3926d11f6c9fd28e6236d50d9c0d59d1dd05cd
ZaraNaeem1234 <zara.naeem@remelda.com>
2026-03-05 16:39:13 +0500

    slash fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/women-event/',
+  base: 'https://myblissmi.com/women-event',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
